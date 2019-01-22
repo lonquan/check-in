4057febf4c7bfe589b44e53871a13cb885ca03aa
tianyong90 <412039588@qq.com>
2019-01-23 00:28:07 +0800

    动效优化

--- src/components/Avatar/Avatar.vue
+++ src/components/Avatar/Avatar.vue
@@ -1,36 +1,145 @@
 <template>
-  <div class="user-avatar">
+  <transition
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+    @after-leave="afterLeave"
+  >
+    <div class="user-avatar-wrapper">
+      <img class="avatar" :src="user.avatar" alt="">
 
-  </div>
+      <div class="welcome-pane" v-if="isAnimating">
+        <img class="welcome-avatar" :src="user.avatar" alt="">
+        <h3 class="welcome-msg">欢迎 {{ user.name }} 大佬！！</h3>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script>
 import 'animate.css'
 import anime from 'animejs'
-import faker from 'faker'
 
 export default {
   name: 'user-avatar',
   props: {
-    msg: String
+    user: Object
   },
 
   data () {
     return {
+      isAnimating: false
     }
   },
+
+  methods: {
+    beforeEnter (el) {
+      this.isAnimating = true
+    },
+
+    enter (el, done) {
+      const bodyRent = document.body.getBoundingClientRect()
+
+      const bodyCenterX = (bodyRent.left + bodyRent.right) / 2
+      const bodyCenterY = (bodyRent.top + bodyRent.bottom) / 2
+
+      const elRect = el.getBoundingClientRect()
+
+      const elCenterX = (elRect.left + elRect.right) / 2
+      const elCenterY = (elRect.top + elRect.bottom) / 2
+
+      console.table(elRect)
+      console.log(elCenterX)
+
+      const deltaX = bodyCenterX - elCenterX
+      const deltaY = bodyCenterY - elCenterY
+
+      const animation = anime.timeline({
+        delay: 0,
+        endDelay: 0,
+        easing: 'easeInOutSine'
+      }).add({
+        targets: el,
+        scale: 2,
+        translateX: deltaX / 2 + 'px', // 除以缩放倍数
+        translateY: deltaY / 2 + 'px'
+      }).add({
+        delay: 3000,
+        targets: el,
+        scale: 1,
+        translateX: '0',
+        translateY: '0'
+      })
+
+      animation.finished.then(() => {
+        done()
+      })
+    },
+
+    afterEnter (el) {
+      this.isAnimating = false
+    },
+
+    beforeLeave (el) {
+
+    },
+
+    leave (el, done) {
+      done()
+    },
+
+    afterLeave (el) {
+
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-$avatar-size: 85px;
+$avatar-size: 100px;
 
-.user-avatar {
+.user-avatar-wrapper {
   display: block;
-  position: fixed;
-  width: 400px;
-  height: 400px;
-  background-color: #fff;
+  position: relative;
+  transform-origin: top center;
+
+  .avatar {
+    display: block;
+    width: $avatar-size;
+    height: $avatar-size;
+    background-color: #f00;
+    border-radius: 50%;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
+  }
+
+  .welcome-pane {
+    display: block;
+    position: absolute;
+    padding: 2rem 0;
+    width: 300px;
+    background-color: #fff;
+    left: -100px;
+    top: -2rem;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
+    border-radius: 4px;
+
+    .welcome-avatar {
+      display: block;
+      margin: 0 auto;
+      width: $avatar-size;
+      height: $avatar-size;
+      background-color: #f00;
+      border-radius: 50%;
+    }
+
+    .welcome-msg {
+      display: block;
+      color: #666;
+      font-size: 1.5rem;
+    }
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,35 @@
 <template>
   <div class="app">
-    <div class="avatar-wall">
-      <div
+    <div class="avatar-wall" @click="addUser">
+      <UserAvatar
         class="avatar-wrapper"
-        :class="{ arrived: user.arrived, ['user-' + user.id]: true }"
-        :ref="'user-' + user.id"
+        :user="user"
         v-for="user in users"
         :key="user.id"
-      >
-        <img class="avatar" :src="user.arrived ? user.avatar : '/default-avatar.jpg'" alt="">
-      </div>
+      />
     </div>
   </div>
 </template>
 
 <script>
-import anime from 'animejs'
 import faker from 'faker'
-
-function getRandomIntInclusive (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; // The maximum is inclusive and the minimum is inclusive
-}
+import UserAvatar from '../components/Avatar/Avatar.vue'
 
 let users = []
 
-for (let i = 1; i < 30; i++) {
+for (let i = 1; i < 10; i++) {
   users.push({
     id: i,
     name: faker.name.firstName(),
-    // avatar: faker.image.avatar(),
-    avatar: '/avatar.jpg',
-    arrived: false
+    avatar: faker.image.image()
   })
 }
 
 export default {
   name: 'HelloWorld',
+
+  components: { UserAvatar },
+
   props: {
     msg: String
   },
@@ -49,73 +41,20 @@ export default {
   },
 
   mounted () {
-    // setInterval(() => {
-    //   const unsignedUsers = this.users.filter(user => user.arrived === false)
-
-    //   const randomIndex = getRandomIntInclusive(0, unsignedUsers.length - 1)
-    //   this.signup(unsignedUsers[randomIndex])
-    // }, 5000)
+    setInterval(() => {
+      this.addUser()
+    }, 5000)
   },
 
   methods: {
-    signup (user) {
-      console.log(user.id)
-      const rect = document.querySelector('.user-' + user.id).getBoundingClientRect()
-
-      const welcomePaneOriginalWidth = 500
-
-      let newEl = document.createElement('div')
-      newEl.classList = 'welcome-pane'
-
-      // 插入头像
-      let imageEl = document.createElement('img')
-      imageEl.src = user.avatar
-      imageEl.classList = 'welcome-avatar'
-
-      newEl.appendChild(imageEl)
-
-      // 插入欢迎词
-      let textEl = document.createElement('p')
-      textEl.classList = 'welcome-msg'
-      textEl.textContent = `Welcome, ${user.name}!`
-
-      newEl.appendChild(textEl)
-
-      document.body.appendChild(newEl)
-
-      const animation = anime.timeline({
-        delay: 0,
-        endDelay: 0,
-        easing: 'easeInOutSine'
-      }).add({
-        targets: newEl,
-        // left: rect.x + 'px',
-        scale: 1.2,
-        duration: 500
-      }).add({
-        targets: textEl,
-        color: '#f00',
-        scale: 0,
-        duration: 100,
-        delay: 3000
-      }).add({
-        targets: newEl,
-        delay: 0,
-        left: rect.x + 'px',
-        top: rect.y,
-        scale: 1,
-        width: '85px',
-        height: '85px',
-        borderRadius: '50%'
+    addUser () {
+      this.users.push({
+        id: this.users.length + 1,
+        name: faker.name.firstName(),
+        avatar: faker.image.image()
       })
-
-      animation.finished.then(() => {
-        this.users[user.id - 1].arrived = true
-        newEl.remove()
-      })
-
-    },
-  },
+    }
+  }
 }
 </script>
 
@@ -127,9 +66,9 @@ $avatar-size: 85px;
   .avatar-wall {
     display: grid;
     width: 65%;
-    overflow: hidden;
     background-color: #f8f8f8;
-    margin: 0 auto;
+    margin: 50px auto;
+    padding: 2rem;
     grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
     grid-gap: 1rem;
 
@@ -147,40 +86,6 @@ $avatar-size: 85px;
 
       }
     }
-  }
-}
-</style>
-
-<style lang="scss">
-.welcome-pane {
-  display: flex;
-  position: fixed;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 0;
-  left: calc(50% - 250px);
-  top: 35%;
-  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.5);
-  border-radius: 3px;
-  width: 500px;
-  overflow: hidden;
-  background-color: #fff;
-  transform-origin: 85px center;
-
-  .welcome-avatar {
-    display: block;
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    margin-top: 2rem;
-  }
-
-  .welcome-msg {
-    display: block;
-    text-align: center;
-    font-size: 2rem;
-    overflow: hidden;
   }
 }
 </style>
