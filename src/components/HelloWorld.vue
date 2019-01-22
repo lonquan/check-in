c6aed7581308803b06a280cb13fb5f7f3e9344b6
tianyong90 <412039588@qq.com>
2019-01-22 17:14:42 +0800

    初步动效

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,133 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="app">
+    <div class="avatar-wall">
+      <div
+        class="avatar-wrapper"
+        :class="{ arrived: user.arrived }"
+        v-for="user in users"
+        :key="user.id"
+        @click="onClick(user.id)"
+      >
+        <img class="avatar" :src="user.avatar" alt="">
+
+        <transition>
+          <div class="welcome-msg">
+            <img class="welcome-avatar" :src="user.avatar" alt="">
+
+            welcome haha
+          </div>
+        </transition>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import faker from 'faker'
+
+let users = []
+
+for (let i = 1; i < 30; i++) {
+  users.push({
+    id: i,
+    name: faker.name,
+    avatar: faker.image.avatar(),
+    arrived: false
+  })
+}
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+
+  data () {
+    return {
+      users
+    }
+  },
+
+  methods: {
+    onClick (id) {
+      this.users[id - 1].arrived = true
+    }
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+$avatar-size: 85px;
+
+.app {
+  display: flex;
+
+  .avatar-wall {
+    display: flex;
+    width: 65%;
+    overflow: hidden;
+    background-color: #f8f8f8;
+    margin: 0 auto;
+    padding: 3rem;
+    flex-wrap: wrap;
+    justify-content: space-around;
+
+    .avatar-wrapper {
+      display: flex;
+      flex-direction: row;
+      margin: 1rem;
+      position: relative;
+
+      .avatar {
+        display: flex;
+        width: $avatar-size;
+        height: $avatar-size;
+        background-color: #fff;
+        border-radius: 50%;
+        padding: 3px;
+        border: 1px solid #ccc;
+
+      }
+
+      .welcome-msg {
+        visibility: hidden;
+        position: absolute;
+        width: $avatar-size;
+        height: $avatar-size;
+        left: 0;
+        top: 0;
+
+        .welcome-avatar {
+          width: $avatar-size;
+          height: $avatar-size;
+          border-radius: 50%;
+        }
+      }
+
+      &.arrived {
+
+
+        .welcome-msg {
+          border-radius: 5px;
+          box-shadow: 0 10px 15px rgba(0, 0, 0, 0.5);
+          visibility: visible;
+          display: block;
+          overflow: hidden;
+          z-index: 999;
+          padding: 3rem;
+          background-color: #fff;
+          left: 300px;
+          position: fixed;
+          transform: scale(2);
+          transition: all 1s ease-in-out;
+
+          .welcome-avatar {
+            transition: all 1s ease-in-out;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
