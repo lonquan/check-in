d69cbcbb12ebf59d6ab54f7b5fb4b7fc98b514d1
tianyong90 <412039588@qq.com>
2019-01-26 22:51:14 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,53 @@
 <template>
   <div class="app">
-    <!--<div id="particles-js"></div>-->
+    <div id="particles-js"></div>
 
-    <!--<h1 class="title shadow-text">嘉宾签到</h1>-->
-    <!--<h2 class="sub-title shadow-text">The guest check in</h2>-->
+    <h1 class="title shadow-text">嘉宾签到</h1>
+    <h2 class="sub-title shadow-text">The guest check in</h2>
 
-    <!--<div class="current-user-info">-->
-      <!--<h1 class="user-name shadow-text">赵龙权</h1>-->
-      <!--<h2 class="user-title shadow-text">蚂蚁创想CEO 原新浪微博 API 架构师</h2>-->
-    <!--</div>-->
+    <!-- <div class="current-user-info">
+      <h1 class="user-name shadow-text">赵龙权</h1>
+      <h2 class="user-title shadow-text">蚂蚁创想CEO 原新浪微博 API 架构师</h2>
+    </div> -->
 
-    <div class="swiper-container">
+    <div class="swiper-container main-swiper">
       <div class="swiper-wrapper">
-        <div class="swiper-slide">Slide 1</div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
-        <div class="swiper-slide">Slide 4</div>
-        <div class="swiper-slide">Slide 5</div>
-        <div class="swiper-slide">Slide 6</div>
-        <div class="swiper-slide">Slide 7</div>
-        <div class="swiper-slide">Slide 8</div>
-        <div class="swiper-slide">Slide 9</div>
-        <div class="swiper-slide">Slide 10</div>
+        <div class="swiper-slide" v-for="user in users" :key="user.id">
+          <div class="avatar-wrapper">
+            <img :src="user.avatar" alt="" class="user-avatar">
+            <h3 class="user-name">{{ user.name }}</h3>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import Vue from 'vue'
+import Vue from 'vue'
 import faker from 'faker'
-// import anime from 'animejs'
-// import 'particles.js'
+import anime from 'animejs'
+import 'particles.js'
 
-// import particleConfig from '../assets/particle-config'
+import particleConfig from '../assets/particle-config'
 
 import Swiper from 'swiper'
 import 'swiper/dist/css/swiper.css'
 
+let users = []
+
+for (let i = 1; i <= 10; i++) {
+  users.push({
+    id: i,
+    // name: faker.name.firstName(),
+    name: 'user-' + i,
+    avatar: '/avatar.jpg'
+  })
+}
+
+
+console.table(users)
 export default {
   name: 'HelloWorld',
 
@@ -45,14 +55,24 @@ export default {
 
   data () {
     return {
+      users
     }
   },
 
   mounted () {
     const mySwiper = new Swiper('.swiper-container', {
-      autoplay: true,
-      direction: 'vertical',
-      loop: true,
+      autoplay: {
+        delay: 2000,
+
+      },
+      loop: false,
+      slidesPerView: 2,
+      slidesPerColumn: 3,
+      slidesPerGroup: 6,
+      slidesPerColumnFill: 'column',
+      // loopFillGroupWithBlank: true,
+      centeredSlides: false,
+      observer: true
     })
 
     // const el = document.querySelector('.center-avatar-wrapper')
@@ -88,88 +108,120 @@ export default {
     // })
 
     /* eslint-disable no-undef */
-    // particlesJS('particles-js', particleConfig)
+    particlesJS('particles-js', particleConfig)
+
+    // setInterval(() => {
+    //   this.addUser()
+    // }, 2000);
   },
 
   methods: {
     addUser () {
-      this.pauseSlide = !this.pauseSlide
+      // this.pauseSlide = !this.pauseSlide
       // TODO
-      // this.users.push({
-      //   id: this.users.length + 1,
-      //   name: faker.name.firstName(),
-      //   avatar: faker.image.image()
-      // })
+      this.users.push({
+        id: this.users.length + 1,
+        name: faker.name.firstName(),
+        // avatar: faker.image.image()
+        avatar: '/default-avatar.jpg'
+      })
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style lang="scss">
   $avatar-size: 85px;
   $center-wrapper-size: 0;
   $center-avatar-size: 60%;
 
+  @keyframes fucking-1 {
+    0% {
+      transform: rotate(0);
+    }
+
+    100% {
+      transform: rotate(-1turn);
+    }
+  }
+
+  @keyframes fucking-2 {
+    0% {
+      transform: rotate(0);
+    }
+
+    25% {
+      transform: rotate(0.35turn);
+    }
+
+    50% {
+      transform: rotate(0.5turn);
+    }
+
+    100% {
+      transform: rotate(0.95turn);
+    }
+  }
+
+  #particles-js {
+    display: block;
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+  }
+
+  h1,
+  h2 {
+    color: #7de7ff;
+  }
+
   .app {
     display: flex;
+    width: 100vw;
+    height: 100vh;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    height: 100vh;
-    width: 100vw;
+    background-image: url(../assets/background.png);
+    background-position: center center;
+    background-size: cover;
   }
 
   .swiper-container {
     width: 80%;
-    height: 60%;
-    background-color: #f00;
-    display: flex;
-  }
+    height: 70%;
 
-  /*.swiper-slide {*/
-    /*width: 100%;*/
-    /*height: 100%;*/
-    /*overflow: hidden;*/
-    /*display: block;*/
-    /*color: #fff;*/
-    /*font-size: 2rem;*/
-  /*}*/
-</style>
+    .swiper-slide {
+      height: 18vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
 
-<style lang="scss">
-  html, body {
-    position: relative;
-    height: 100%;
-  }
-  body {
-    background: #eee;
-    font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
-    font-size: 14px;
-    color:#000;
-    margin: 0;
-    padding: 0;
-  }
-  .swiper-container {
-    width: 100%;
-    height: 100%;
-    background-color: #f00;
-  }
-  .swiper-slide {
-    text-align: center;
-    font-size: 18px;
-    background: #f00;
-    /* Center slide text vertically */
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: -webkit-flex;
-    display: flex;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    -webkit-justify-content: center;
-    justify-content: center;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    -webkit-align-items: center;
-    align-items: center;
+    .avatar-wrapper {
+      display: flex;
+      flex-direction: column;
+      overflow: hidden;
+      padding: 0;
+      margin: 0;
+      justify-content: center;
+      align-items: center;
+
+      .user-avatar {
+        display: block;
+        width: 10vh;
+        height: 10vh;
+        overflow: hidden;
+        border-radius: 50%;
+      }
+
+      .user-name {
+        color: #fff;
+      }
+    }
+
   }
 </style>
